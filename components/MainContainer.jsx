968fc976e0742a76d60972384864cdc2485f4cb1
amine alaoui <omari1amineco@gmail.com>
2024-01-03 18:30:20 +0100

    fix: style in MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -4,13 +4,13 @@ import React from "react";
 const MainContainer = () => {
   return (
     <div>
-      <div className="mb-10 flex justify-center">
+      <div className="mb-8 flex justify-center">
         <button className="rounded-full border bg-zinc-700 px-5 py-2 text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl">
           Get a New Quote
         </button>
       </div>
-      <div className="space-y-5 rounded-xl border border-zinc-700 p-5">
-        <h1 className="p-4 text-center text-2xl font-medium">
+      <div className="space-y-5 rounded-xl border border-zinc-700 p-6">
+        <h1 className="p-3 text-center text-2xl font-medium">
           quote:this is the quote
         </h1>
         <div>
