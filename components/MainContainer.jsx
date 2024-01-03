79781137d41f34eeae0fbf1d223849fb1a467a06
amine alaoui <omari1amineco@gmail.com>
2024-01-03 19:12:24 +0100

    fix: style in MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -3,22 +3,22 @@ import React from "react";
 
 const MainContainer = () => {
   return (
-    <div>
+    <div className="w-full">
       <div className="mb-8 flex justify-center">
-        <button className="rounded-full border bg-zinc-700 px-5 py-2 text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl">
+        <button className="rounded-full border text-sm md:text-base bg-zinc-700 px-6 py-2 text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl">
           Get a New Quote
         </button>
       </div>
-      <div className="space-y-5 rounded-xl border border-zinc-700 p-6">
-        <h1 className="p-3 text-center text-2xl font-medium">
+      <div className="space-y-3 rounded-xl border border-zinc-700 p-6">
+        <h1 className="rounded-xl bg-zinc-700 py-3 px-8 text-center font-medium text-white">
           quote:this is the quote
         </h1>
         <div>
-          <p className="text-sm font-medium">Author :</p>
-          <p className="text-xs">Amine Omari Alaoui</p>
+          <p className="text-xs font-medium">Author :</p>
+          <p className="text-[10px]">Amine Omari Alaoui</p>
         </div>
         <div className="flex justify-end">
-          <button className="rounded-full border hover:border-zinc-700 p-1 duration-300 hover:bg-white">
+          <button className="rounded-full border p-1 duration-300 hover:border-zinc-700 hover:bg-white">
             <ShareIcon />
           </button>
         </div>
