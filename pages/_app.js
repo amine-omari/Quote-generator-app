b4f725b7a9f5e2441cc2d7912bf6b5a912c752a2
amine alaoui <omari1amineco@gmail.com>
2024-01-03 21:00:05 +0100

    fix: _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="syste" attribute="class">
+    <ThemeProvider defaultTheme="system" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   );
