8ce9c32c26f4a0e43b47576d7058c74614541cd0
amine alaoui <omari1amineco@gmail.com>
2024-01-03 21:27:11 +0100

    feat: import SunIcon and MoonIcon to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
 import MainContainer from "@/components/MainContainer";
+import MoonIcon from "@/icons/MoonIcon";
+import SunIcon from "@/icons/SunIcon";
 import { useTheme } from "next-themes";
 
 export default function Home() {
-  const [resolvedTheme, theme, setTheme] = useTheme();
+  const { resolvedTheme, theme, setTheme } = useTheme();
+  console.log(resolvedTheme, theme, setTheme);
   return (
     <main className="mx-2 flex h-screen items-center justify-center">
       <button
+      className="absolute top-10"
         onClick={() => {
           setTheme(resolvedTheme === "light" ? "dark" : "light");
         }}
-      ></button>
+      >
+        {theme === "light" ? <MoonIcon /> : <SunIcon />}
+      </button>
       <MainContainer />
     </main>
   );
