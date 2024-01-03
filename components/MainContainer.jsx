92f039d0e5697c8c695642f874e49ae93f0f8893
amine alaoui <omari1amineco@gmail.com>
2024-01-03 19:24:47 +0100

    feat: add responsiveness to the page

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const MainContainer = () => {
   return (
-    <div className="w-full">
+    <div className="w-full max-w-[550px]">
       <div className="mb-8 flex justify-center">
-        <button className="rounded-full border text-sm md:text-base bg-zinc-700 px-6 py-2 text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl">
+        <button className="rounded-full border bg-zinc-700 px-6 py-2 text-sm text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl md:text-lg">
           Get a New Quote
         </button>
       </div>
-      <div className="space-y-3 rounded-xl border border-zinc-700 p-6">
-        <h1 className="rounded-xl bg-zinc-700 py-3 px-8 text-center font-medium text-white">
+      <div className="space-y-3 rounded-xl border border-zinc-700 p-3 md:p-6">
+        <h1 className="rounded-xl bg-zinc-700 p-3 text-center font-medium text-white md:text-2xl">
           quote:this is the quote
         </h1>
         <div>
-          <p className="text-xs font-medium">Author :</p>
-          <p className="text-[10px]">Amine Omari Alaoui</p>
+          <p className="text-xs font-medium md:text-sm">Author :</p>
+          <p className="text-[8px] md:text-xs">Amine Omari Alaoui</p>
         </div>
         <div className="flex justify-end">
           <button className="rounded-full border p-1 duration-300 hover:border-zinc-700 hover:bg-white">
