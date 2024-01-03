8ff15ed787e889d0d32444a67339c1253293b35a
amine alaoui <omari1amineco@gmail.com>
2024-01-03 17:05:23 +0100

    feat: add a button to the page

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -3,6 +3,9 @@ import React from "react";
 const MainContainer = () => {
   return (
     <div>
+      <div className="flex justify-center">
+        <button>Get a New Quote</button>
+      </div>
       <div className="space-y-5">
         <h1 className="text-2xl text-center font-medium">
           quote:this is the quote
