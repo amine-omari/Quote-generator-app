17a8356732cfc2cbc6e50668aeb297aa171e817b
amine alaoui <omari1amineco@gmail.com>
2024-01-03 17:43:05 +0100

    feat: add hover style to the button

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const MainContainer = () => {
   return (
     <div>
-      <div className="flex justify-center">
-        <button>Get a New Quote</button>
+      <div className="mb-10 flex justify-center">
+        <button className="rounded-full border bg-zinc-700 px-5 py-2 text-white duration-300 hover:border-zinc-700 hover:bg-white hover:text-zinc-700 hover:shadow-2xl">
+          Get a New Quote
+        </button>
       </div>
       <div className="space-y-5">
-        <h1 className="text-center text-2xl font-medium">
+        <h1 className="rounded-xl bg-zinc-700 p-4 text-center text-2xl font-medium text-white">
           quote:this is the quote
         </h1>
         <div>
-          <p className="text-lg font-medium">Author :</p>
-          <p>Amine Omari Alaoui</p>
+          <p className="font-medium">Author :</p>
+          <p className="text-sm">Amine Omari Alaoui</p>
         </div>
       </div>
     </div>
