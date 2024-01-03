f40d2a703b05dfbfc4a6bdbafa516d0ae5a60692
amine alaoui <omari1amineco@gmail.com>
2024-01-03 20:59:53 +0100

    feat: add useTheme to the index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import MainContainer from "@/components/MainContainer";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const [resolvedTheme, theme, setTheme] = useTheme();
   return (
-    <main className="flex h-screen items-center justify-center mx-2">
+    <main className="mx-2 flex h-screen items-center justify-center">
+      <button
+        onClick={() => {
+          setTheme(resolvedTheme === "light" ? "dark" : "light");
+        }}
+      ></button>
       <MainContainer />
     </main>
   );
